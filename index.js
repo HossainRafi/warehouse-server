7e3a3c8a58ecf55de470ecaa1326bea4d5abdbc8
HossainRafi <hossainrafi71@gmail.com>
2022-05-06 20:27:24 +0600

    get all data

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwdre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+async function run() {
+  try {
+    await client.connect();
+    const mobileCollection = client.db("warehouse").collection("mobile");
+    app.get("/mobiles", async (req, res) => {
+      const query = {};
+      const cursor = mobileCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("server is running")
-})
+  res.send("server is running");
+});
 
 app.listen(port, () => {
-    console.log("server is running in the command line",port);
-})
+  console.log("server is running in the command line", port);
+});
